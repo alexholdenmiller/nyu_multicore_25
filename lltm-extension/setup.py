5c6a054d33b60169cf10cf04f5b1dcab2dbac208
Jack Pang <45305339+Jackpanger@users.noreply.github.com>
2023-04-15 22:47:39 -0400

    compile modification

--- lltm-extension/setup.py
+++ lltm-extension/setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
-setup(name='lltm_cpp',
-      ext_modules=[cpp_extension.CppExtension('lltm_cpp', ['lltm.cpp'], extra_compile_args=['-fopenmp'])],
+
+# original codes
+# setup(name='lltm_cpp',
+#       ext_modules=[cpp_extension.CppExtension('lltm_cpp', ['lltm.cpp'], extra_compile_args=['-fopenmp'])],
+#       cmdclass={'build_ext': cpp_extension.BuildExtension})
+
+
+setup(name='cpp_extension',
+      ext_modules=[
+            cpp_extension.CppExtension('lltm_cpp', ['lltm.cpp'], extra_compile_args=['-fopenmp']),
+            cpp_extension.CppExtension('base_cpp', ['base_forward.cpp'], extra_compile_args=['-fopenmp'])
+      ],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
